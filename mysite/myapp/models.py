942b7d4614f927fbd523e26572fadaff58e86e9b
Eunice <115462849+Estemetek@users.noreply.github.com>
2024-05-15 01:28:36 +0800

    Update models.py

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Student(models.Model):
-    whole_name=models.CharField(max_length=100)
-    age = models.IntegerField()
-    course = models.CharField(max_length=5)
-    gender = models.CharField(max_length=1)
+# class Student(models.Model):
+#     whole_name=models.CharField(max_length=100)
+#     age = models.IntegerField()
+#     course = models.CharField(max_length=5)
+#     gender = models.CharField(max_length=1)
 
 class Add(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     age = models.IntegerField()
-    course = models.CharField(max_length=4)
+    course = models.CharField(max_length=5)
     gender = models.CharField(max_length=2)
