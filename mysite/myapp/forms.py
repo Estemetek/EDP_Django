807b1ec4434802e6b33b26d2822992996952b506
Eunice <115462849+Estemetek@users.noreply.github.com>
2024-05-15 01:29:13 +0800

    Update forms.py

--- mysite/myapp/forms.py
+++ mysite/myapp/forms.py
@@ -11,14 +11,15 @@ GENDER = (
 )
 
 class StudentForm(forms.Form):
-    whole_name=forms.CharField(required=True)
-    age = forms.IntegerField(required=True)
-    course = forms.ChoiceField(widget=forms.RadioSelect, choices = COURSES)
-    gender = forms.ChoiceField(widget=forms.RadioSelect, choices = GENDER)
+    full_name=forms.CharField(required=False)
+    age1 = forms.IntegerField(required=False, min_value=1)
+    age2 = forms.IntegerField(required=False, min_value=1)
+    course = forms.ChoiceField(widget=forms.RadioSelect, choices = COURSES, required=False)
+    gender = forms.ChoiceField(widget=forms.RadioSelect, choices = GENDER, required=False)
 
 class addStudent(forms.Form):
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
-    age = forms.IntegerField(required=True)
-    course = forms.ChoiceField( choices = COURSES)
+    age = forms.IntegerField(required=True, min_value=1)
+    course = forms.ChoiceField(required=True, widget=forms.Select, choices = COURSES)
     gender = forms.ChoiceField(widget=forms.RadioSelect, choices = GENDER)
