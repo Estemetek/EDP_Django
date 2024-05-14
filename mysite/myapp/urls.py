8997dd0da73f4b7107fa0992b2242620a0a90a2b
Eunice <115462849+Estemetek@users.noreply.github.com>
2024-05-15 01:29:54 +0800

    Update urls.py

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    # path("", views.home, name="home"),
+
+    path('', views.filter_table, name="index"),
+    
+    path('add/', views.home, name="add"),
+
+    path('add/<int:id>/', views.update, name="update"),
+
+    path("delete/<int:id>/", views.delete, name="delete"),
+
+    path('filter/', views.filter_table, name="filter")
 ]
