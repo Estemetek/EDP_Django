071844d3378793f8ff76c1ad2dae9b6b62f28651
Eunice <115462849+Estemetek@users.noreply.github.com>
2024-05-15 01:30:46 +0800

    ty cmates ily all hihi

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Student
+# from .models import Student
 from .models import Add
 
 # Register your models here.
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    pass
+# @admin.register(Student)
+# class StudentAdmin(admin.ModelAdmin):
+#     pass
 
 @admin.register(Add)
 class AddAdmin(admin.ModelAdmin):
